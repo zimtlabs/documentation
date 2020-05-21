1b02c565a5ff981422837f67954237e187ec48c7
Lazar <lazareric.com@gmail.com>
2020-05-21 15:20:55 +0200

    GA

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,6 +1,7 @@
 
 class Config {
     default = {
+        env: process.env.NODE_ENV,
         api: {
             core: 'https://hub.zi.mt',
             version: 'v1.0.0',
@@ -10,6 +11,7 @@ class Config {
 
     get config() {
         return {
+            dev: this.default.env === 'dev' || this.default.env === 'development',
             api: {
                 core: process.env.REACT_APP_API_CORE || this.default.api.core,
                 version: process.env.REACT_APP_API_API_VERSION || this.default.api.version,
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { config } from 'dotenv';
+import GA from 'react-ga';
 
 import 'prismjs/themes/prism.css';
 import 'prismjs/themes/prism-okaidia.css';
@@ -9,9 +10,26 @@ import 'nprogress/nprogress.css';
 
 import * as serviceWorker from './serviceWorker';
 import App from './app/App';
+import Config from './app/config';
+import { history } from './app/utils';
 
 const init = () => {
     config();
+    const dev = Config.config.dev;
+
+    // Inits in production
+    if (!dev) {
+        GA.initialize('UA-157314622-4');
+        // Initial app open pageview record
+        GA.pageview(window.location.pathname + window.location.search);
+        // Routing record
+        history.listen(location => {
+            // Update the user's current page
+            GA.set({ page: location.pathname });
+            // Record a pageview for the given page
+            GA.pageview(location.pathname + location.search);
+        });
+    }
 };
 
 init();
